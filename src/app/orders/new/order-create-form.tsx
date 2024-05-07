47fbb445981268e677679cd50d269b1b4ab7f6f4
sukebon <39957486+sukebon@users.noreply.github.com>
2024-05-07 23:14:26 +0900

    collectiongroup orderby delete

--- src/app/orders/new/order-create-form.tsx
+++ src/app/orders/new/order-create-form.tsx
@@ -56,7 +56,7 @@ export default function OrderCreateForm() {
           { id: doc.id, ...doc.data() } as Product)
         );
 
-        const skusRef = query(collectionGroup(db, "skus"), orderBy("sortNum", "asc"));
+        const skusRef = collectionGroup(db, "skus");
         const skusSnap = await getDocs(skusRef);
 
         const skus = skusSnap.docs
