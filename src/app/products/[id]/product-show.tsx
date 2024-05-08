e04ec50fdb403054d1daba1cf17e5040bb42767b
daimaru-hakui <dh.mukai.dh@gmail.com>
2024-05-08 15:53:47 +0900

    product show return icon

--- src/app/products/[id]/product-show.tsx
+++ src/app/products/[id]/product-show.tsx
@@ -1,20 +1,72 @@
+"use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductShowTable from "./product-show-table";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "@/lib/firebase/client";
+import { Product } from "@/types";
+import { ArrowLeft, ChevronLeft, ChevronRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface Props {
   id: string;
 }
 
 export default function ProductShow({ id }: Props) {
-  
+  const [product, setProduct] = useState<Product>();
+  const router = useRouter();
+
+  const handlePageBack = () => {
+    router.back();
+  };
+
+  useEffect(() => {
+    const productRef = doc(db, "products", id);
+    const unsub = onSnapshot(productRef, {
+      next: (snapshot) => {
+        setProduct(snapshot.data() as Product);
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+    return () => {
+      unsub();
+    };
+  }, [id]);
+
+  console.log(product);
+
   return (
     <Card className="w-full md:w-[450px]">
       <CardHeader>
+        <div className="flex justify-between mb-4">
+          <ArrowLeft className="cursor-pointer" onClick={handlePageBack} />
+          <span className="flex gap-3 ml-auto">
+            <ChevronLeft className="cursor-pointer" />
+            <ChevronRight className="cursor-pointer" />
+          </span>
+        </div>
         <CardTitle>詳細</CardTitle>
       </CardHeader>
       <CardContent>
-        <ProductShowTable id={id} />
+        <div className="flex flex-col gap-1">
+          <div className="flex gap-3 text-sm">
+            <div>表示名</div>
+            <div>{product?.displayName}</div>
+          </div>
+          <div className="flex gap-3 text-sm">
+            <div>品　番</div>
+            <div>{product?.productNumber}</div>
+          </div>
+          <div className="flex gap-3 text-sm">
+            <div>品　名</div>
+            <div>{product?.productName}</div>
+          </div>
+        </div>
+        <div className="mt-3">
+          <ProductShowTable id={id} />
+        </div>
       </CardContent>
     </Card>
   );
