86e3f722f81b51e18f3e78efa29e7e2c7f591147
daimaru-hakui <dh.mukai.dh@gmail.com>
2024-09-20 10:42:24 +0900

    title change demo

--- src/components/layouts/sidebar/MenuTitle.tsx
+++ src/components/layouts/sidebar/MenuTitle.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function MenuTitle() {
   return (
     <div className="text-lg px-2 ">
-      <Link href="/">TSUCHIYA APP</Link>
+      <Link href="/">DEMO</Link>
     </div>
   );
 }
